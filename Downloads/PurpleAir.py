70e5f9a44b0bdde56168abd51e3f3ea8ee0207a5
Daniel Monroy <fernandodms04@gmail.com>
2024-07-26 17:12:05 -0600

    Add PA documents for download

--- Downloads/PurpleAir.py
+++ Downloads/PurpleAir.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-GarzaLaguera = pd.read_csv("PA/TEC_Garza_Laguera.csv", parse_dates=["time_stamp"])
+GarzaLaguera = pd.read_csv("PA/TEC_Garza_Laguera_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+Apodaca = pd.read_csv("PA/Apodaca_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+Juarez = pd.read_csv("PA/Juarez_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+SanNicolas = pd.read_csv("PA/San Nicolas_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+SanPedro = pd.read_csv("PA/San Pedro_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+SantaCatarina = pd.read_csv("PA/Santa Catarina_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
 
-Names = ["Tec Garza Laguera"]
-Municipalities = [GarzaLaguera]
+Names = ["Tec Garza Laguera", "Apodaca", "Juarez", "San Nicolas", "San Pedro", "Santa Catarina"]
+Municipalities = [GarzaLaguera, Apodaca, Juarez, SanNicolas, SanPedro, SantaCatarina]
 
 Mun_Dict = dict(zip(Names, Municipalities))
 
