1c62d3c8b41925d29ff95f833971dfeb98125b70
Daniel Monroy <fernandodms04@gmail.com>
2024-07-26 18:01:39 -0600

    Fix: Rename PA docs

--- Downloads/PurpleAir.py
+++ Downloads/PurpleAir.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-GarzaLaguera = pd.read_csv("PA/TEC_Garza_Laguera_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
-Apodaca = pd.read_csv("PA/Apodaca_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
-Juarez = pd.read_csv("PA/Juarez_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
-SanNicolas = pd.read_csv("PA/San Nicolas_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
-SanPedro = pd.read_csv("PA/San Pedro_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
-SantaCatarina = pd.read_csv("PA/Santa Catarina_API_60MINS_2022-2024.csv", parse_dates=["time_stamp"])
+GarzaLaguera = pd.read_csv("PA/TEC_Garza_Laguera_PA.csv", parse_dates=["time_stamp"])
+Apodaca = pd.read_csv("PA/Apodaca_PA.csv", parse_dates=["time_stamp"])
+Juarez = pd.read_csv("PA/Juarez_PA.csv", parse_dates=["time_stamp"])
+SanNicolas = pd.read_csv("PA/SanNicolas_PA.csv", parse_dates=["time_stamp"])
+SanPedro = pd.read_csv("PA/SanPedro_PA.csv", parse_dates=["time_stamp"])
+SantaCatarina = pd.read_csv("PA/SantaCatarina_PA.csv", parse_dates=["time_stamp"])
 
 Names = ["Tec Garza Laguera", "Apodaca", "Juarez", "San Nicolas", "San Pedro", "Santa Catarina"]
 Municipalities = [GarzaLaguera, Apodaca, Juarez, SanNicolas, SanPedro, SantaCatarina]
